6ff31e03a12e737c6b51f4a31b3432303269e203
Bocahapel <pratamadrian.putra@gmail.com>
2025-10-13 18:02:19 +0700

    update user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+//import { Component, computed, signal } from '@angular/core';
+//import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+//const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,13 +12,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // userImage = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  @Input({ required: true }) avatar!: string; // ! is for telling typescript that we will assign the value without ts knowing where
+  @Input({ required: true }) name!: string;
 
   get userImage() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.avatar;
   }
-
   onSelectUser() {
-    console.log('Click');
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
