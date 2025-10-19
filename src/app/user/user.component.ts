75d11b5f6c34e627e8c9417ae74049e7159bb3e3
Bocahapel <pratamadrian.putra@gmail.com>
2025-10-19 12:47:21 +0700

    update user component
    Add Input Output event emitter for user ID

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,6 @@
 //import { Component, computed, signal } from '@angular/core';
 //import { DUMMY_USERS } from '../dummy-users';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, input, Input, Output } from '@angular/core';
 
 //const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
@@ -15,8 +15,10 @@ export class UserComponent {
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
   // userImage = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string; // ! is for telling typescript that we will assign the value without ts knowing where
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
   //avatar = input.required<string>();  <-- this is how to use input signal instead of @Input
 
@@ -26,5 +28,6 @@ export class UserComponent {
   onSelectUser() {
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
   }
 }
